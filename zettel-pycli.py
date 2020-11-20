74b1d4c47530523b31644a16c7467bed682ec8e7
roal-yr <roal.yr@gmail.com>
2020-11-20 17:12:28 +0200

    ref and clean

--- zettel-pycli.py
+++ zettel-pycli.py
@@ -51,163 +51,67 @@ marker_body, marker_tags, marker_links, ''])
 create_meta_table = '''
 	CREATE TABLE IF NOT EXISTS meta (
 		id integer PRIMARY KEY,
-		db_name text NOT NULL,
-		datetime text NOT NULL,
-		tot_zettels integer NOT NULL,
-		tot_links integer NOT NULL,
-		tot_invalid_links integer NOT NULL,
-		tot_no_links integer NOT NULL,
-		tot_self_links integer NOT NULL,
-		tot_no_bodies integer NOT NULL,
+		db_name text NOT NULL, datetime text NOT NULL,
+		tot_zettels integer NOT NULL, tot_links integer NOT NULL,
+		tot_invalid_links integer NOT NULL, tot_no_links integer NOT NULL,
+		tot_self_links integer NOT NULL, tot_no_bodies integer NOT NULL,
 		tot_no_titles integer NOT NULL
 	); '''
-
 create_main_table = '''
 	CREATE TABLE IF NOT EXISTS main (
-		id integer PRIMARY KEY,
-		z_title text NOT NULL,
-		z_path text NOT NULL,
-		z_body text NOT NULL
+		id integer PRIMARY KEY, z_title text NOT NULL,
+		z_path text NOT NULL, z_body text NOT NULL
 	); '''
-
 create_links_table = '''
 	CREATE TABLE IF NOT EXISTS links (
 		id integer PRIMARY KEY,
-		z_id_from integer NOT NULL,
-		z_path_to integer NOT NULL
+		z_id_from integer NOT NULL, z_path_to integer NOT NULL
 	); '''
-	
 create_invalid_links_table = '''
 	CREATE TABLE IF NOT EXISTS invalid_links (
 		id integer PRIMARY KEY,
-		z_id_from integer NOT NULL,
-		z_path_to text NOT NULL
+		z_id_from integer NOT NULL, z_path_to text NOT NULL
 	); '''
-
 create_no_links_table = '''
 	CREATE TABLE IF NOT EXISTS no_links (
-		id integer PRIMARY KEY,
-		z_id_from integer NOT NULL
+		id integer PRIMARY KEY, z_id_from integer NOT NULL
 	); '''
-	
 create_self_links_table = '''
 	CREATE TABLE IF NOT EXISTS self_links (
-		id integer PRIMARY KEY,
-		z_id_from integer NOT NULL
+		id integer PRIMARY KEY, z_id_from integer NOT NULL
 	); '''
-	
 create_empties_table = '''
 	CREATE TABLE IF NOT EXISTS empties (
-		id integer PRIMARY KEY,
-		z_id_from integer NOT NULL
+		id integer PRIMARY KEY, z_id_from integer NOT NULL
 	); '''
-	
 create_titleless_table = '''
 	CREATE TABLE IF NOT EXISTS titleless (
-		id integer PRIMARY KEY,
-		z_id_from integer NOT NULL
+		id integer PRIMARY KEY, z_id_from integer NOT NULL
 	); '''
-
 create_tags_table = '''
 	CREATE TABLE IF NOT EXISTS tags (
-		id integer PRIMARY KEY,
-		z_id integer NOT NULL,
-		tag text NOT NULL
+		id integer PRIMARY KEY, z_id integer NOT NULL, tag text NOT NULL
 	); '''
 
+insert_main = '''INSERT INTO main ( z_title, z_path, z_body ) VALUES ( ?, ?, ? ) '''
+insert_links = '''INSERT INTO links ( z_id_from, z_path_to ) VALUES ( ?, ? ) '''
+insert_invalid_links = '''INSERT INTO invalid_links ( z_id_from, z_path_to ) VALUES ( ?, ? ) '''
+insert_no_links = '''INSERT INTO no_links ( z_id_from ) VALUES ( ? ) '''
+insert_self_links = '''INSERT INTO self_links ( z_id_from ) VALUES ( ? ) '''
+insert_empties = '''INSERT INTO empties ( z_id_from ) VALUES ( ? ) '''
+insert_titleless = '''INSERT INTO titleless ( z_id_from ) VALUES ( ? ) '''
+insert_tags = '''INSERT INTO tags ( z_id, tag ) VALUES ( ?, ? ) '''
 insert_meta = '''
 	INSERT INTO meta (
-		db_name,
-		datetime,
-		tot_zettels,
-		tot_links,
-		tot_invalid_links,
-		tot_no_links,
-		tot_self_links,
-		tot_no_bodies,
-		tot_no_titles
-	)
-	VALUES(
-		?, ?, ?, ?, ?, ?, ?, ?, ?
-	) '''
-
-insert_main = '''
-	INSERT INTO main (
-		z_title,
-		z_path,
-		z_body
-	)
-	VALUES(
-		?, ?, ?
-	) '''
-
-insert_links = '''
-	INSERT INTO links (
-		z_id_from,
-		z_path_to
-	)
-	VALUES(
-		?, ?
-	) '''
-	
-insert_invalid_links = '''
-	INSERT INTO invalid_links (
-		z_id_from,
-		z_path_to
-	)
-	VALUES(
-		?, ?
-	) '''
-	
-insert_no_links = '''
-	INSERT INTO no_links (
-		z_id_from
-	)
-	VALUES(
-		?
-	) '''
-	
-insert_self_links = '''
-	INSERT INTO self_links (
-		z_id_from
-	)
-	VALUES(
-		?
-	) '''
-	
-insert_empties = '''
-	INSERT INTO empties (
-		z_id_from
-	)
-	VALUES(
-		?
-	) '''
-	
-insert_titleless = '''
-	INSERT INTO titleless (
-		z_id_from
-	)
-	VALUES(
-		?
-	) '''
-	
-insert_tags = '''
-	INSERT INTO tags (
-		z_id,
-		tag
-	)
-	VALUES(
-		?, ?
-	) '''
+		db_name, datetime, tot_zettels, tot_links, tot_invalid_links,
+		tot_no_links, tot_self_links, tot_no_bodies, tot_no_titles
+	) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? ) '''
 	
 #Just fancy stuff
-banner_git	  = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒GIT MENU▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
-banner_log	  = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒LOG▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
-banner_commit   = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒COMMITTING▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
-banner_revert   = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒REVERTING▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
-banner_hreset   = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒HARD RESETTING▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
-banner_main	 = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒MAIN MENU▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
-banner_zettel_ops	 = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ZETTEL ACTIONS▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
+banner_log	  = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ LOG ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
+banner_commit   = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ COMMITTING ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
+banner_revert   = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ REVERTING ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
+banner_hreset   = '▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ HARD RESETTING ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒'
 divider = '-------------------------------------------------------'
 lorem = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 Phasellus mollis vulputate lobortis. Etiam auctor, massa in pulvinar 
@@ -229,22 +133,18 @@ git_add = "git add . && git status --short"
 git_push = "git push --all"
 
 def print_git_ops():
-	print('')
-	print(banner_git)
+	print(divider)
 	print('() - current')
 	print('(l) - log')
 	print('(s) - status')
-	print('')
 	print('(a) - add')
 	print('(c) - commit')
 	print('(p) - push')
-	print('')
 	print('(r) - revert')
 	print('(ha) - hard reset')
-	print(banner_git)
-	print('')
-	print('(u) - launch "gitui"')
+	print('(u) - launch "gitui" (must be installed)')
 	print('(q) - quit to main')
+	print(divider)
 
 def git_log_f():
 	print(banner_log)
@@ -261,7 +161,7 @@ def git_commit_f():
 	commit_name = input("New commit name (' ' to abort) » ").strip()
 	if commit_name =="":
 		return
-	inp = input("Really commit? » ").strip()
+	inp = input("Really? (type 'yes') » ").strip()
 	if inp == "yes":
 		git_commit = "git commit -m "+commit_name
 		os.system(git_commit)
@@ -285,43 +185,29 @@ def git_reset_hard_f():
 	commit_name = input("Reset to commit name (' ' to abort) » ").strip()
 	if commit_name =="":
 		return
-	inp = input("Really? » ").strip()
+	inp = input("Really? (type 'yes') » ").strip()
 	if inp == "yes":
 		git_reset_hard = "git reset --hard "+commit_name
 		os.system(git_reset_hard)
 	
 #Begin
 def git_menu():
+	os.system('clear')
 	print_git_ops()
 	while True:
-		print('')
 		inp = input("GIT MENU ('?' for commands) » ").strip()
-		print('')
-				
-		if inp == "l":
-			git_log_f()
-		elif inp == "":
-			print('Current head:')
-			os.system(git_log_1)
-		elif inp == "s":
-			os.system(git_status)
-		elif inp == "a":
-			os.system(git_add)
-		elif inp == "c":
-			git_commit_f()
-		elif inp == "p":
-			os.system(git_push)
-		elif inp == "r":
-			git_revert_f()
-		elif inp == "ha":
-			git_reset_hard_f()
-		elif inp == "?":
-			print_git_ops()
-		elif inp == "u":
-			os.system('gitui')
-		elif inp == "q":
-			break
-			
+		os.system('clear')
+		if inp == "": print('Current head:'); os.system(git_log_1)
+		elif inp == "l": git_log_f()
+		elif inp == "s": os.system(git_status)
+		elif inp == "a": os.system(git_add)
+		elif inp == "c": git_commit_f()
+		elif inp == "p": os.system(git_push)
+		elif inp == "r": git_revert_f()
+		elif inp == "ha": git_reset_hard_f()
+		elif inp == "?": print_git_ops()
+		elif inp == "u": os.system('gitui')
+		elif inp == "q": break
 			
 
 #▒▒▒▒▒▒▒▒▒▒▒▒ FILE OPS ▒▒▒▒▒▒▒▒▒▒▒▒▒
@@ -390,7 +276,6 @@ def make_test_batch():
 				+ '\n\n' + marker_body + '\n' + marker_tags + '\n' \
 				+ "test, zettel batch, performance" + '\n\n' + marker_links + '\n' + links
 		
-		
 		f = open(path + "/" + str(i) + '.md', "w")
 		f.write(zettel_template_test)
 		f.close()
@@ -401,7 +286,6 @@ def make_new():
 	f.write(zettel_template_test)
 	f.close()
 	return inp
-
 
 
 #▒▒▒▒▒▒▒▒▒▒▒▒ PARSING OPS ▒▒▒▒▒▒▒▒▒▒▒▒▒
@@ -416,15 +300,7 @@ def find_comma_separated(md):
 	return text
 	
 def parse_zettel_metadata(z_path):
-	#expected parsed data
-	data = {
-		'title' : '',
-		'body' : '',
-		'tags' : [],
-		'links' : [],
-	}
-	
-	# open the file and read through it line by line
+	data = {'title' : '', 'body' : '', 'tags' : [], 'links' : [], }
 	f = open(z_path, 'r')
 	
 	#a switch flag to read links in tge end of the file
@@ -464,17 +340,10 @@ def parse_zettel_metadata(z_path):
 			reading_links = True
 			continue
 		
-		if reading_title:
-			data['title'] += line.strip()
-			
-		if reading_body:
-			data['body'] += line.strip()
-			
-		if reading_tags:
-			data['tags'] += find_comma_separated(line)
-			
-		if reading_links:
-			data['links'] += find_md_links(line)
+		if reading_title: data['title'] += line.strip()
+		if reading_body: data['body'] += line.strip()
+		if reading_tags: data['tags'] += find_comma_separated(line)
+		if reading_links: data['links'] += find_md_links(line)
 	
 	return data
 	
@@ -638,12 +507,7 @@ def update_db():
 			conn.close()
 
 
-
 #▒▒▒▒▒▒▒▒▒▒▒▒ ANALYSIS OPS ▒▒▒▒▒▒▒▒▒▒▒▒▒
-def print_search_commands():
-	print("'number' - select entry")
-	print("(c) - clear search query")
-	
 def print_entries(entry, val):
 	z_title = entry[1]
 	z_path = entry[2]
@@ -656,7 +520,7 @@ def print_entries(entry, val):
 		print()
 		print('selected:', str(val)+'.', z_path)
 	print_zettel_ops()
-	zettel_ops(z_id)
+	zettel_ops(z_id, z_path)
 
 def increm_input_title_name():
 	name = ''
@@ -664,41 +528,29 @@ def increm_input_title_name():
 	entities = []
 	while True:
 		num = 0
-		inp = input('srarching for: ' + name + " « ")
+		
 		#see if we are entering the name or command
 		if inp != ':':
 			os.system('clear')
 			print(divider)
 			name += inp
 		
-		#show all if no input
+		#show all if no input, or find by name or title
 		if name == '':
 			get_all = "SELECT * FROM main"
 			entries = query_db(get_all)
-		#or find by name or title
-		if name != '':
+		else:
 			get_by_name = "SELECT * FROM main WHERE z_path LIKE '%"+name+"%' OR z_title LIKE '%"+name+"%'"
 			entries = query_db(get_by_name)
 		
-		#commands sub-menu
+		#search sub-menu
 		if inp == ':':
-			print(divider)
-			print_search_commands()
-			i = input(" » ")
-			print()
-			print(divider)
-			try:
-				val = int(i)
-				print_entries(entries[val], val)
-			except:
-				pass
-			finally:
-				if i == "c":
-					name = ''
-					inp = ''
+			if search_ops(entries): 
+				name = ''; inp = '' #resets
+				entries = query_db(get_all) #resets
 			os.system('clear')
-			continue
 		
+		#printing out entries
 		if len(entries) > 1:
 			for entry in entries:
 				if entry[1] != '':
@@ -710,39 +562,31 @@ def increm_input_title_name():
 					print()
 					print(str(num)+'.', entry[2])
 				num += 1
-				
 			print(divider)
 			print('total search hits:', len(entries))
 			print('keep narrowing your search by entering more characters')
 			print("or enter ':' for search tools")
 			print(divider)
-			
 		elif len(entries) == 1: 
 			print_entries(entries[0], '')
-		
-		#break if neither name matches
 		elif len(entries) == 0: 
 			print('no zettel found, returning to main menu')
 			print(divider)
 			return
 		
+		inp = input('srarching for: ' + name + " « ")
 		
 def list_corrupt_links():
 	get_all = "SELECT * FROM invalid_links"
 	entries = query_db(get_all)
-	if entries == []:
-		return False
-		
+	if entries == []: return False
 	same_file = False
 	name_previous = ''
 	num = 0
 	for entry in entries:
 		get_file_name = "SELECT * FROM main WHERE id =" + str(entry[1])
 		name = query_db(get_file_name)[0][2]
-
-		if name == name_previous:
-			same_file = True
-			
+		if name == name_previous: same_file = True
 		if same_file:
 			print('   └─', entry[2])
 			same_file = False
@@ -752,14 +596,12 @@ def list_corrupt_links():
 			print('   corrupt links:')
 			print('   └─', entry[2])
 			num += 1
-		
 		name_previous = name
 	return True
 		
 def list_zettels(exec_str):
 	entries = query_db(exec_str)
-	if entries == []:
-		return False
+	if entries == []: return False
 	num = 0
 	for entry in entries:
 		get_file_name = "SELECT * FROM main WHERE id =" + str(entry[1])
@@ -767,39 +609,50 @@ def list_zettels(exec_str):
 		print(str(num)+'.', 'file:', name)
 		num += 1
 	return True
-	
+
+#▒▒▒▒▒▒▒▒▒▒▒▒ SEARCH MENU ▒▒▒▒▒▒▒▒▒▒▒▒▒
+def print_search_commands():
+	print("'number' - select entry")
+	print("(c) - clear search query")
+
+def search_ops(entries):
+	print(divider)
+	print_search_commands()
+	i = input(" » ")
+	print()
+	print(divider)
+	try: print_entries(entries[int(i)], int(i))
+	except: pass
+	finally:
+		if i == "c": return True #reset query input
+
+#▒▒▒▒▒▒▒▒▒▒▒▒ ZETTEL OPS ▒▒▒▒▒▒▒▒▒▒▒▒▒
 def print_zettel_body(z_id):
 	get_body = "SELECT * FROM main WHERE id =" + str(z_id)
 	body = query_db(get_body)
+	print(divider)
 	print(body[0][3])
+	print(divider)
 	
-	
-
-#▒▒▒▒▒▒▒▒▒▒▒▒ ZETTEL OPS ▒▒▒▒▒▒▒▒▒▒▒▒▒
 def print_zettel_ops():
-	print('')
-	print(banner_zettel_ops)
+	print(divider)
 	print('() - read current zettel')
-	print(banner_zettel_ops)
-	print('')
 	print('(q) - quit')
+	print(divider)
 	
-def zettel_ops(z_id):
+def zettel_ops(z_id, z_path):
+	os.system('clear')
+	print_zettel_ops()
 	while True:
+		print('selected zette:', z_path)
 		inp = input("ZETTEL OPS ('?' for commands) » ").strip()
-		if inp == "":
-			os.system('clear')
-			print(divider)
-			print_zettel_body(z_id)
-			print(divider)
-		elif inp == "?":
-			print_zettel_ops()
-		elif inp == 'q':
-			return
+		os.system('clear')
+		if inp == "": print_zettel_body(z_id)
+		elif inp == "?": print_zettel_ops()
+		elif inp == 'q': return
 		
-		
-			
-#▒▒▒▒▒▒▒▒▒▒▒▒ FUNCTION WRAPPERS ▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+#▒▒▒▒▒▒▒▒▒▒▒▒ MAIN MENU ▒▒▒▒▒▒▒▒▒▒▒▒▒
 def sync():
 	print(divider)
 	update_db()
@@ -841,37 +694,32 @@ def review():
 	print(divider)
 	print()
 	if list_zettels("SELECT * FROM titleless"):
-		print(divider)
+		print()
 		print('there are zettels without titles listed above, inspect')
 		print(divider)
 		print()
 		errors = True
-		
 	if list_zettels("SELECT * FROM empties"):
-		print(divider)
+		print()
 		print('there are zettels without text listed above, fill them')
 		print(divider)
 		print()
 		errors = True
-		
 	if list_zettels("SELECT * FROM no_links"):
-		print(divider)
+		print()
 		print('there are unlinked zettels listed above, link them')
 		print(divider)
 		errors = True
-	
 	if list_zettels("SELECT * FROM self_links"):
-		print(divider)
+		print()
 		print('there are zettels linking to themselves listed above')
 		print(divider)
 		errors = True
-	
 	if list_corrupt_links():
-		print(divider)
+		print()
 		print('there are corrupt links in your zettels, review them')
 		print(divider)
 		errors = True
-		
 	if not errors:
 		print('all good, no corrupt links or unlinked zettels')
 		print(divider)
@@ -892,67 +740,36 @@ def make_test_zettels():
 	print("don't forget to update the database")
 	print(divider)
 	
-		
-		
-#▒▒▒▒▒▒▒▒▒▒▒▒ MAIN MENU ▒▒▒▒▒▒▒▒▒▒▒▒▒
 def print_main_ops():
-	print('')
-	print(banner_main)
+	print(divider)
 	print('() - show statistics')
 	print('(u) - update the database')
 	print('(f) - incrementally find zettel to enter the graph')
 	print('(r) - review zettels for errors in links')
 	print('(tree) - use "tree" command to show files')
-	print()
 	print('(n) - make new empty zettel')
 	print('(temp) - generate a template zettel')
 	print('(test) - generate a batch of test zettels')
-	print(banner_main)
-	print('')
 	print('(t) - git menu')
 	print('(q) - quit')
-		
-def main_menu():
-	print_main_ops()
+	print(divider)
 	
+def main_menu():
+	os.system('clear')
+	info()
 	while True:
-		print('')
 		inp = input("MAIN MENU ('?' for commands) » ").strip()
-		print('')
-				
-		if inp == "":
-			os.system('clear')
-			info()
-		if inp == "u":
-			os.system('clear')
-			sync()
-		if inp == "n":
-			os.system('clear')
-			make_new_zettel()
-		if inp == "f":
-			os.system('clear')
-			find_zettel()
-		if inp == "r":
-			os.system('clear')
-			review()
-		if inp == "tree":
-			os.system('clear')
-			tree()
-		if inp == "temp":
-			os.system('clear')
-			make_template()
-		if inp == "test":
-			os.system('clear')
-			make_test_zettels()
-		elif inp == "t":
-			os.system('clear')
-			git_menu()
-		elif inp == "?":
-			print_main_ops()
-		elif inp == "q":
-				quit()
+		os.system('clear')
+		if inp == "": info()
+		elif inp == "u": sync()
+		elif inp == "n": make_new_zettel()
+		elif inp == "f": find_zettel()
+		elif inp == "r": review()
+		elif inp == "tree": tree()
+		elif inp == "temp": make_template()
+		elif inp == "test": make_test_zettels()
+		elif inp == "t": git_menu()
+		elif inp == "?": print_main_ops()
+		elif inp == "q": quit()
 
-#Start 
-os.system('clear')
-while True:
-	main_menu()
+main_menu()
